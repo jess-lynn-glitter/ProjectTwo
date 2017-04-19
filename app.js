80fd8966d3a75ce0cfdea86e9c111a0ac589d9a6
alexandroidd <alexandroidcreighton@gmail.com>
2017-04-19 14:27:12 -0600

    adds revamped file layout and beginning of passport

---
+++ app.js
@@ -0,0 +1,29 @@
+var express      = require('express');
+var app          = express();
+var mongoose     = require('mongoose');
+var passport     = require('passport');
+var flash        = require('connect-flash');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+mongoose.connect('mongodb://localhost/SurfSpot_Project'); 
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+
+app.set('views', './views');
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+var routes = require('./config/routes');
+app.use(routes);
+
+app.list(3000);
---
+++ controllers/users.js
@@ -0,0 +1,20 @@
+var passport = require('passport');
+
+//GET /signup
+function getSignup(request, response, next) {}
+
+//POST /signup
+function postSignup(request, response, next){}
+
+//GET /login
+function getLogin(request, response) {}
+
+//POST /login
+function postLogin(request, response, next) {}
+
+//GET /logout
+function getLogout(request, response){}
+
+//Restricted page
+function secret(request, response){}
+
