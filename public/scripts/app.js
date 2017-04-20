6e762c41567fbc6c677393aea14fcbcb83d83069
alexandroidd <alexandroidcreighton@gmail.com>
2017-04-20 16:09:50 -0600

    this adds a functional info window whose text can change

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,67 +4,93 @@ console.log("app is working!");
 
 
 function initMap() {
-        var bakersBeach = {lat: 41.508354, lng: -71.070333};
-        var theBirdSanctuary = {lat: 41.507436, lng: -71.020238};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 13,
-          center: bakersBeach
-        });
-        var marker = new google.maps.Marker({
-          position: bakersBeach,
-          map: map
-        });
+    var bakersBeach = {lat: 41.508354, lng: -71.070333};
+    var theBirdSanctuary = {lat: 41.507436, lng: -71.020238};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 13,
+      center: bakersBeach
+    });
+    
+    var marker = new google.maps.Marker({
+      position: bakersBeach,
+      map: map
+    });
 
-        var birdMarker = new google.maps.Marker({
-        	position: theBirdSanctuary,
-        	map: map
-        });
+    
+     var infoWindow = new google.maps.InfoWindow({
+      content: "this shouldn't be here"
+    });
 
-        map.addListener('click', function(e) {
-          placeMarkerAndPanTo(e.latLng, map);
-        });
+    var birdMarker = new google.maps.Marker({
+    	position: theBirdSanctuary,
+    	map: map,
+      title: 'The Bird Sanctuary'
+    });
 
-        function placeMarkerAndPanTo(latLng, map) {
-          var marker = new google.maps.Marker({
-            position: latLng,
-            map: map
-          });
-
-          var spotLat = latLng.lat();
-          var spotLng = latLng.lng();
-
-          var spotDetails = {
-            name: 'SurfSpotName',
-            lat: spotLat,
-            lng: spotLng,
-            currentConditions: 'Head-High'
-          };
-
-          $.ajax({
-            url:'/surfspots',
-            type: 'POST',
-            data: spotDetails,
-            success:
-            console.log("Sucess")
-          });
-
-          map.panTo(latLng);
-          console.log(spotLat);
-          console.log(spotLng);  //// this accesses the lat (and lng if needed) of the point created.
-          
-        }
-
- // $.ajax({
- //        url: '/api/albums',
- //        type: 'POST',
- //        data: formData,
- //        success:
- //        function makeAlbum(data) {
- //          renderAlbum(data);
- //         $(this).trigger('reset');
-        // function saveSurfData()
+    birdMarker.addListener('click', function() {
+      infoWindow.open(map, birdMarker);
+      infoWindow.setContent("THIS IS CONTENT JUST FOR BIRD SANC");
+    });
+   
 
 
+    map.addListener('click', function(e) {
+      // placeMarkerAndPanTo(e.latLng, map);
+
+// if window is open) then close it and return) else add a new spot
+
+    });
+
+//      var infowindow = new google.maps.InfoWindow({
+//     content: contentString
+//   });
+
+//   var marker = new google.maps.Marker({
+//     position: uluru,
+//     map: map,
+//     title: 'Uluru (Ayers Rock)'
+//   });
+//   marker.addListener('click', function() {
+//     infowindow.open(map, marker);
+//   });
+// }
+
+
+
+
+
+
+
+
+    function placeMarkerAndPanTo(latLng, map) {
+      var marker = new google.maps.Marker({
+        position: latLng,
+        map: map
+      });
+
+      var spotLat = latLng.lat();
+      var spotLng = latLng.lng();
+
+      var spotDetails = {
+        name: 'SurfSpotName',
+        lat: spotLat,
+        lng: spotLng,
+        currentConditions: 'Head-High'
+      };
+
+      $.ajax({
+        url:'/surfspots',
+        type: 'POST',
+        data: spotDetails,
+        success:
+        console.log("Sucess")
+      });
+
+      map.panTo(latLng);
+      console.log(spotLat);
+      console.log(spotLng);  //// this accesses the lat (and lng if needed) of the point created.
+      
+    }
 
 }
 
