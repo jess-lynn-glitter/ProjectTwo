dfd17145edc6ff3f86e3b82831b3a171555501e4
alexandroidd <alexandroidcreighton@gmail.com>
2017-04-24 09:42:46 -0600

    simple edits

--- models/index.js
+++ models/index.js
@@ -7,4 +7,3 @@ mongoose.connect(process.env.MONGODB_URI ||
 
 module.exports.User = require('./user');
  module.exports.Surfspot = require('./surfSpots.js');
-// module.exports.Campsite = require("./campsite.js.example");
